c0635cf15f19d26a541c4d3cc90fda2ec5c7b39c
Aluno <aluno@eteclencois.com.br>
2019-04-08 11:44:43 -0300

    Criação do menu

--- aulaapi-UI/src/app/app.component.ts
+++ aulaapi-UI/src/app/app.component.ts
@@ -8,11 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Projeto de Aula';
 
-  categorias = [
-    {id:'1', nome: 'Informática'},
-    {id:'2', nome: 'Escritório'},
-    {id:'3', nome: 'Lanchonete'},
-    {id:'4', nome: 'Refeitório'},  
   
-  ]
 }
